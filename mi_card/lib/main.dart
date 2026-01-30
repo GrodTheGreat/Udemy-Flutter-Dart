cba98b5c91b9685d33eb08a1a7b1dbbcce27319b
Gerard Causse <grod.causse@gmail.com>
2026-01-30 02:28:19 -0500

    How to use Column & Row for Layout

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(backgroundColor: Colors.teal, body: Container()),
-    ),
-  );
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  const MiCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.white,
+                width: 100.0,
+                height: 100.0,
+                child: Text('Container 1'),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100.0,
+                height: 100.0,
+                child: Text('Container 2'),
+              ),
+              Container(
+                color: Colors.white,
+                width: 100.0,
+                height: 100.0,
+                child: Text('Container 3'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
