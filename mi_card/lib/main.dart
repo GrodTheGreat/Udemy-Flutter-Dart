7db23f8338c4b8c8170a97f2a77da19c85e2d762
Gerard Causse <grod.causse@gmail.com>
2026-01-31 14:59:36 -0500

    Adding Material Icons with the Icon Widget

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -15,17 +15,10 @@ class MiCard extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    foregroundImage: AssetImage(
-                      'assets/images/ravelord-nito.jpg',
-                    ),
-                    radius: 50.0,
-                    backgroundColor: Colors.red,
-                  ),
-                ],
+              CircleAvatar(
+                foregroundImage: AssetImage('assets/images/ravelord-nito.jpg'),
+                radius: 50.0,
+                backgroundColor: Colors.red,
               ),
               Text(
                 'John Doe',
@@ -44,6 +37,44 @@ class MiCard extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   letterSpacing: 2.5,
                   fontFamily: 'SourceSans3',
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.teal),
+                    SizedBox(width: 10),
+                    Text(
+                      '+1 (555) 555-5555',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: 'SourceSans3',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(width: 10),
+                    Text(
+                      'john_doe@email.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: 'SourceSans3',
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
