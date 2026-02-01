19ff78b6150bdee0d3e199e3450867c46c49e5c4
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2026-01-31 23:39:41 -0500

    Complete Dicee project (#4)
    * Dicee - A Stateful Dice App
    
    * Using the Expanded Widget to Create Flexible Layouts
    
    * Adding Gesture Detection with Flutter Button Widgets
    
    * Stateful vs Stateless Widgets - What's the difference?
    
    * Randomizing the Dice
    
    * Complete challenges

---
+++ dicee/lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  int rollDie() {
+    return Random().nextInt(6) + 1;
+  }
+
+  void rollDice() {
+    setState(() {
+      leftDiceNumber = rollDie();
+      rightDiceNumber = rollDie();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextButton(
+                onPressed: rollDice,
+                child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextButton(
+                onPressed: rollDice,
+                child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
