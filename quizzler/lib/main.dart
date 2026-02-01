23477272b74b10d15b976b38959a47cc44c10a3d
Gerard Causse <grod.causse@gmail.com>
2026-02-01 11:55:33 -0500

    Displaying the Questions

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -25,6 +25,12 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int questionNumber = 0;
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    "A slug's blood is green.",
+  ];
   List<Icon> scoreKeeper = [
     Icon(Icons.check, color: Colors.green),
     Icon(Icons.close, color: Colors.red),
@@ -42,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -61,6 +67,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+                  questionNumber++;
                 });
               },
             ),
@@ -78,6 +85,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+                  questionNumber++;
                 });
               },
             ),
