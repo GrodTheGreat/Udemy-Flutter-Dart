8282ecca18556997249711a79820ad070953f9f0
GrodTheGreat <60365291+GrodTheGreat@users.noreply.github.com>
2026-02-01 10:24:45 -0500

    Complete Xylophone project (#6)
    * Set up Xylophone project
    
    * What are Flutter & Dart Packages?
    
    * How to Play Multiple Sounds
    
    * Refactor Our App
    
    * Arrow Functions

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  final player = AudioPlayer();
+
+  XylophoneApp({super.key});
+
+  void playSound(int soundNumber) {
+    player.play(AssetSource('sounds/note$soundNumber.wav'));
+  }
+
+  Widget buildKey(Color color, int soundNumber) => Expanded(
+    child: TextButton(
+      style: TextButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        backgroundColor: color,
+      ),
+      onPressed: () => playSound(soundNumber),
+      child: Container(),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.blue, 5),
+              buildKey(Colors.indigo, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
